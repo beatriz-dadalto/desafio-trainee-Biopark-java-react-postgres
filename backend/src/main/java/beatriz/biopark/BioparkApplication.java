2c0b7b4a893c595142002894097617a28f6ef901
Beatriz Dadalto <biacoelho@proton.me>
2023-03-02 20:58:57 -0300

    Backend project created

---
+++ backend/src/main/java/beatriz/biopark/BioparkApplication.java
@@ -0,0 +1,13 @@
+package beatriz.biopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioparkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/beatriz/biopark/BioparkApplicationTests.java
@@ -0,0 +1,13 @@
+package beatriz.biopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
